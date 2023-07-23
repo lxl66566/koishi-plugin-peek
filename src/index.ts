a7854b7e6fe64870c3d57460d7fd1bf0d608bdc7
lxl66566 <18259734087@163.com>
2023-07-24 01:10:04 +0800

    Implement screenshot and blur functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,49 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from "koishi";
+import screenshot from "screenshot-desktop";
+import sharp from "sharp";
 
-export const name = 'peek'
+export const name = "peek";
+export interface Config {
+  blur: number;
+}
+export const Config: Schema<Config> = Schema.object({
+  blur: Schema.number()
+    .default(0)
+    .description("Gaussian Blur's radius, 0 means no blur"),
+});
 
-export interface Config {}
+async function applyGaussianBlur(
+  imageBuffer: Buffer,
+  radius: number
+): Promise<Buffer> {
+  if (radius === 0) {
+    return imageBuffer;
+  }
+  try {
+    const resultBuffer = await sharp(imageBuffer).blur(radius).toBuffer();
+    return resultBuffer;
+  } catch (error) {
+    console.error("Error applying Gaussian blur:", error);
+    throw error;
+  }
+}
 
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  ctx.on("message", (session) => {
+    if (session.content === "peek") {
+      screenshot()
+        .then((imageString) => {
+          return applyGaussianBlur(
+            Buffer.from(imageString, "base64"),
+            config.blur
+          );
+        })
+        .then((imageBuffer) => {
+          session.send(h.image(imageBuffer, "image/png"));
+        })
+        .catch((e) => {
+          session.send(e);
+        });
+    }
+  });
 }
